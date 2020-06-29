c342b1a6413a7e37222d908528d9aeb3ac4f3b2b
Thales Reis <thales.reis-sys@parceiros-kroton.com.br>
2020-06-29 12:29:37 -0300

    first commit

---
+++ meu-micro-app-a/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: '/' },
+  ],
+})
+export class AppRoutingModule { }
